f4a0e15a3409a5a5ebef6d83d3fab179293eefba
ALPINISTLAR <niveam424@gmail.com>
2024-01-07 10:31:57 +0200

    add features section

--- js/main.js
+++ js/main.js
@@ -8,5 +8,36 @@ function visibilityNone() {
   elLauncher.style.visibility = "hidden";
 }
 
-  setTimeout(closeLauncher, 1500);
-  setTimeout(visibilityNone, 2300);
+setTimeout(closeLauncher, 1500);
+setTimeout(visibilityNone, 2300);
+
+// =============
+const elsTabsItem = document.querySelectorAll('.tabs__item');
+const elsTabLink = document.querySelectorAll('.js-tab-link');
+const elsTabpanels = document.querySelectorAll('.tabpanels__item');
+
+
+function DeactivateTabItems () {
+  elsTabsItem.forEach(function (elTabsItem) {
+    elTabsItem.classList.remove('tabs__item--active');
+  });
+}
+
+function DeactivateTabpanels () {
+  elsTabpanels.forEach(function (elTabpanel) {
+    elTabpanel.classList.remove('tabpanels__item--active');
+  });
+}
+
+
+elsTabLink.forEach(function (elTabLink) {
+  elTabLink.addEventListener('click', function (evt) {
+    evt.preventDefault();
+    DeactivateTabItems();
+    elTabLink.parentElement.classList.add('tabs__item--active');
+    DeactivateTabpanels();
+    // const elTargetPanel = document.querySelector(`#${elTabLink.href.split('#')[1]}`);
+    const elTargetPanel = document.querySelector(elTabLink.dataset.tabTarget);
+    elTargetPanel.classList.add('tabpanels__item--active');
+  });
+});
