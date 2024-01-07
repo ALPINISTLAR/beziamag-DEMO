ad9a1cd1a6ddc42c5d1c80b7db8c1e70dc24b1ee
ALPINISTLAR <niveam424@gmail.com>
2024-01-07 19:07:38 +0200

    add products section

--- js/main.js
+++ js/main.js
@@ -41,3 +41,47 @@ elsTabLink.forEach(function (elTabLink) {
     elTargetPanel.classList.add('tabpanels__item--active');
   });
 });
+
+// =================
+
+document.querySelector('.btn--right').addEventListener('click', function() {
+  const productList = document.querySelector('.products__list');
+  // Boshqa talablar bo'yicha shu erda scroll qilishingiz mumkin
+  productList.scrollLeft += 100; // Masalan, har safar 100 piksel o'nga o'chadi
+});
+
+document.querySelector('.btn--left').addEventListener('click', function() {
+  const productList = document.querySelector('.products__list');
+  // Boshqa talablar bo'yicha shu erda scroll qilishingiz mumkin
+  productList.scrollLeft -= 100; // Masalan, har safar 100 piksel o'nga o'chadi
+});
+
+
+let intervalId;
+
+function startMoving(direction) {
+  const productList = document.querySelector('.products__list');
+  const speed = 2; // Harakatlanish tezligi (px)
+
+  intervalId = setInterval(function() {
+    if (direction === 'right') {
+      productList.scrollLeft += speed;
+    } else if (direction === 'left') {
+      productList.scrollLeft -= speed;
+    }
+  }, 10); // 10 millisekundda bir harakatlanish
+}
+
+function stopMoving() {
+  clearInterval(intervalId);
+}
+
+document.querySelector('.btn--left').addEventListener('mousedown', function() {
+  startMoving('left');
+});
+
+document.querySelector('.btn--right').addEventListener('mousedown', function() {
+  startMoving('right');
+});
+
+document.addEventListener('mouseup', stopMoving);
