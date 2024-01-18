608d393655d8289133b5307c45190546d8c135f7
ALPINISTLAR <niveam424@gmail.com>
2024-01-18 15:21:44 +0200

    turn off launcher (for a short time)

--- js/main.js
+++ js/main.js
@@ -1,15 +1,3 @@
-const elLauncher = document.querySelector('.launcher');
-
-function closeLauncher() {
-  elLauncher.classList.add('launcher--close');
-}
-
-function visibilityNone() {
-  elLauncher.style.visibility = "hidden";
-}
-
-setTimeout(closeLauncher, 1500);
-setTimeout(visibilityNone, 2300);
 
 // =============
 const elsTabsItem = document.querySelectorAll('.tabs__item');
