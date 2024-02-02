fe1d61a6728c6564499b0089de1b3d64c0a4ba02
ALPINISTLAR <niveam424@gmail.com>
2024-02-02 22:49:48 +0200

    products page

--- js/main.js
+++ js/main.js
@@ -47,6 +47,7 @@ var sticky = header.offsetTop;
 const hero = document.querySelector('.hero');
 const contactPage = document.querySelector('.contact-page');
 const aboutPage = document.querySelector('.about-page');
+const productsInner = document.querySelector('.products__inner');
 
 
 function stickyHeader() {
@@ -60,6 +61,9 @@ function stickyHeader() {
     };
     if (aboutPage) {
       aboutPage.style.paddingTop = '240px';
+    };
+    if (productsInner) {
+      productsInner.style.paddingTop = '190px';
     }
   } else {
     header.classList.remove("sticky");
@@ -72,43 +76,43 @@ function stickyHeader() {
     if (aboutPage) {
       aboutPage.style.paddingTop = '';
     }
+    if (productsInner) {
+      productsInner.style.paddingTop = '';
+    }
   }
 }
-
-
 
 // == Product cards
 function productCard(product) {
   return `<li class="products__list-item product-card">
-  <img class="product-card__img" src="${product.image}" alt="${product.title}">
+  <a> <img class="product-card__img" src="${product.thumbnail}" alt="${product.title}">
   <div class="product-card__info">
-  <h3 class="product-card__title">${product.title}</h3>
-  <span class="product-card__price">$. ${product.price}</span>
-  </div>
+    <h3 class="product-card__title">${product.title}</h3>
+    <span class="product-card__price">$${product.price}</span>
+  </div></a>
   </li>`;
 }
 
-async function appendCard(count) {
+async function appendCard(skip, count) {
   const elProductList = document.querySelector('.products__list');
   let cardsHtml = '';
 
   if (elProductList) {
     try {
       // API dan ma'lumotlarni olish
-      const res = await fetch("https://fakestoreapi.com/products");
-      const data = await res.json();
+      const res = await fetch("https://dummyjson.com/products");
+      const jsonData = await res.json();
 
-      if (!res.ok) {
-        throw new Error(`Ma'lumotlarni olishda xato: ${data.message}`);
-      }
+      // Agar ma'lumotlar "products" nomli maydon ichida bo'lsa
+      const data = jsonData.products;
 
-      // Faqat 8 ta ma'lumotni olish
-      const limitedData = data.slice(10, 18);
+      // Faqat count ta ma'lumotni olish
+      const limitedData = data.slice(skip, count);
 
       // Har bir mahsulot uchun cardHtml ni qo'shish
-      for (let i = 0; i < count; i++) {
-        cardsHtml += productCard(limitedData[i]);
-      }
+      limitedData.forEach((product) => {
+        cardsHtml += productCard(product);
+      });
 
       // ProductList ga cardHtml ni qo'shish
       elProductList.innerHTML += cardsHtml;
@@ -116,8 +120,7 @@ async function appendCard(count) {
       console.error("Ma'lumotlarni olishda xatolik:", error);
     }
   }
-
 }
 
-// Misol uchun 8 ta productCard joylash
-appendCard(8);
+// appendCard funksiyasini chaqirish
+// appendCard(8);
